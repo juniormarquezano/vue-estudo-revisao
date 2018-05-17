07d9b5fa27e8d70f3c189fbd10af218c50e1a18f
Junior Marquezano <juniormarquezano@gmail.com>
2018-05-16 23:31:27 -0300

    Instalação do bootstrap via sass

---
+++ src/components/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="jumbotron">
+    <h1>Olá Mundo!</h1>
+    <p>Projeto de estudo do vuejs.</p>
+  </div>
+</template>
---
+++ src/components/root/header/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">CYBER</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">PRODUTOS</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
